9740f51ecf10fe50e99a14b22e7315d6882a74f4
Victor <fy123nig@gmail.com>
2025-01-10 09:46:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,3 +22,4 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/content="${explain}${header}"/content="${content}${explain}${header}"/g' /usr/share/wechatpush/wechatpush
